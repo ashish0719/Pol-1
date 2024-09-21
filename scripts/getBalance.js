fcdf9624e8da4fb452da2e9a0bb01b60d243c416
ashish0719 <130694720+ashish0719@users.noreply.github.com>
2024-09-21 17:04:32 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 const NFTJson = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 
-const NFtAddress = "0xF6Edf6914A612b4CC30dBcF8187cB98Dfe024fe2";
+const NFtAddress = "0x2a38FFC8c5A5F495C0a62d7d79630034D5E1ad03";
 const ContractABI = NFTJson.abi;
 const walletAddress = "0x57b02e207c5b665E02bD2867ae2071Ec6bccB5eB";
 
